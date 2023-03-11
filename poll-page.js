5636031f5f796451022bfed44b986f977d107600
swalloich <nels.jac42@gmail.com>
2023-03-10 19:35:32 -0700

    added github logo link to footer. some work on js

--- poll-page.js
+++ poll-page.js
@@ -1,4 +1,22 @@
+//#region Global variables
+    let polls = null;
+//#endregion
+
+function initiatePage()
+{
+    console.log("initiatePage called");
+}
+
 function showPollModal()
 {
     $('#poll-add-modal').modal('show');
+}
+
+function addPoll()
+{
+    const pollTitleElement = document.getElementById('poll-title');
+    const pollSDateElement = document.getElementById('start-date');
+    const pollSTimeElement = document.getElementById('start-time');
+    const pollEDateElement = document.getElementById('end-date');
+    const pollETimeElement = document.getElementById('end-time');
 }
